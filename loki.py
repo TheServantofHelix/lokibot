e463a8a591f34eb28ae1a4ae9b48b2468fb2bc53
TheServantofHelix <43733207+TheServantofHelix@users.noreply.github.com>
2018-10-01 12:22:53 -0300

    Add files via upload

--- loki.py
+++ loki.py
@@ -4,7 +4,6 @@ import pdb
 import re
 import os
 import time
-import datetime
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Create the Reddit instance
@@ -19,26 +18,40 @@ def bot_login():
     return reddit
 
 def bot_run(reddit):
+    #reddit.login(REDDIT_USERNAME, REDDIT_PASS)
+    # Have we run this code before? If not, create an empty list
+    if not os.path.isfile(dir_path + "\posts_replied_to.txt"):
+        posts_replied_to = []
+    # If we have run the code before, load the list of posts we have replied to
+    else:
+        # Read the file into a list and remove any empty values
+        with open(dir_path + "\posts_replied_to.txt", "r") as f:
+            posts_replied_to = f.read()
+            posts_replied_to = posts_replied_to.split("\n")
+            posts_replied_to = list(filter(None, posts_replied_to))
     # Get the top 25 values from our subreddit
     print ("Getting 25 submissions")
     subreddit = reddit.subreddit('smite')
     for submission in subreddit.new(limit=25):
         print(submission.title)
-        subtime = submission.created_utc
-        cyctime = datetime.datetime.time(datetime.datetime.now())
-        waittime = (00:00:10.000000)
-        print ("'" + submission.title + "' submitted on:")
-        print (subtime)
-        print ("Current cycle time:")
-        print (cyctime)
-        if subtime < (cyctime - waittime):
+        # If we haven't replied to this post before
+        print ("Checking if we have replied to " + submission.title)
+        if submission.id not in posts_replied_to:
             search = submission.title.lower() + submission.selftext.lower()
             if ('loki' in search and 'rework' in search) or ('loki' in search and 'broken' in search) or ('loki' in search and 'overpowered' in search) or ('loki' in search and 'unfun' in search):
                 reply = open("reply.txt", "r")
                 submission.reply(reply.read())
                 print("Bot replying to : ", submission.title.lower())
-    print ("Sleeping...")
-    time.sleep(waittime)
+                # Store the current id into our list
+                print ("Storing " + submission.id + " in posts_replied_to.txt")
+                posts_replied_to.append(submission.id)
+                print (posts_replied_to)
+    print ("Writing list back to file")
+    with open(dir_path + "\posts_replied_to.txt", "w") as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + "\n")
+    print ("Sleeping for 10 seconds...")
+    time.sleep(10)
 
 reddit = bot_login()
 while True:
