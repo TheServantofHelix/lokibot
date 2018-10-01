0400ca79b40663637b659ea0e24055e3eadf3133
TheServantofHelix <43733207+TheServantofHelix@users.noreply.github.com>
2018-10-01 15:10:35 -0300

    Add files via upload

--- loki.py
+++ loki.py
@@ -4,7 +4,9 @@ import pdb
 import re
 import os
 import time
+import psycopg2
 dir_path = os.path.dirname(os.path.realpath(__file__))
+DATABASE_URL = os.environ['DATABASE_URL']
 
 # Create the Reddit instance
 def bot_login():
@@ -15,37 +17,36 @@ def bot_login():
                      user_agent='LokiBot v0.1',
                      username='LokiBot')
     print ("Logged in.")
+    print ("Creating SQL Database")
+    table = (
+    CREATE DATABASE lokibot;
+    CREATE TABLE posts_replied_to (
+    ids ARRAY NOT NULL )
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    cur.execute(table)
     return reddit
+
 
 def bot_run(reddit):
     #reddit.login(REDDIT_USERNAME, REDDIT_PASS)
-    # Have we run this code before? If not, create an empty list
-    if not os.path.isfile(dir_path + "\posts_replied_to.txt"):
-        posts_replied_to = []
-    # If we have run the code before, load the list of posts we have replied to
-    else:
-        # Read the file into a list and remove any empty values
-        with open(dir_path + "\posts_replied_to.txt", "r") as f:
-            posts_replied_to = f.read()
-            posts_replied_to = posts_replied_to.split("\n")
-            posts_replied_to = list(filter(None, posts_replied_to))
     # Get the top 25 values from our subreddit
     print ("Getting 25 submissions")
-    subreddit = reddit.subreddit('smite')
+    subreddit = reddit.subreddit('lokicsstest')
     for submission in subreddit.new(limit=25):
         print(submission.title)
         # If we haven't replied to this post before
         print ("Checking if we have replied to " + submission.title)
-        if submission.id not in posts_replied_to:
+        getids = SELECT ids FROM posts_replied_to;
+        if submission.id not in getids:
             search = submission.title.lower() + submission.selftext.lower()
             if ('loki' in search and 'rework' in search) or ('loki' in search and 'broken' in search) or ('loki' in search and 'overpowered' in search) or ('loki' in search and 'unfun' in search):
                 reply = open("reply.txt", "r")
                 submission.reply(reply.read())
                 print("Bot replying to : ", submission.title.lower())
                 # Store the current id into our list
-                print ("Storing " + submission.id + " in posts_replied_to.txt")
-                posts_replied_to.append(submission.id)
-                print (posts_replied_to)
+                print ("Storing " + submission.id + " database")
+                INSERT INTO posts_replied_to(ids) VALUES (submission.id)
     print ("Sleeping for 10 seconds...")
     time.sleep(10)
 
