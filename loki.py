9418982557155db0ddef6639e7129df1007834dc
TheServantofHelix <43733207+TheServantofHelix@users.noreply.github.com>
2018-10-12 07:25:43 -0300

    Add files via upload

--- loki.py
+++ loki.py
@@ -47,6 +47,6 @@ print ("Creating SQL Database")
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = conn.cursor()
-cur.execute("CREATE TABLE posts_replied_to (ids text, PRIMARY KEY(ids));")
+cur.execute("CREATE TABLE IF NOT EXISTS posts_replied_to (ids text, PRIMARY KEY(ids));")
 while True:
     bot_run(reddit)
