3e67a03775cfef3936b942f095ea176f369a782d
TheServantofHelix <43733207+TheServantofHelix@users.noreply.github.com>
2018-10-12 09:35:41 -0300

    Add files via upload

--- loki.py
+++ loki.py
@@ -38,7 +38,7 @@ def bot_run(reddit):
                 # Store the current id into our list
                 print ("Storing " + submission.id + "in the database")
                 subid = submission.id
-                cur.execute("UPDATE posts_replied_to SET ids = || subid")
+                cur.execute(f"UPDATE posts_replied_to SET ids = (|| '{subid}')")
     print ("Sleeping for 10 seconds...")
     time.sleep(10)
 
